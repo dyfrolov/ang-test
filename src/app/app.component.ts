8230c624302a4d7b85747d452a4c8bec96ab6ff8
Dmitry <dyfrolovfrol@gmail.com>
2022-03-17 00:18:47 +0200

    first

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
-
+import * as moment from 'moment';
+import { Moment } from 'moment';
+import { getMatch, IPMatch, IPSubnetwork, IPRange, matches } from 'ip-matching';
+import {Netmask} from 'netmask';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +10,47 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'test';
+  ip:string = "";
+  ipmask:string = "";
+  selectedCar: number = 1;
+  selectedCars: number[] = [1];
+  selected: {startDate: Moment, endDate: Moment}={startDate:moment(),endDate:moment()};
+  cars = [
+      { id: 1, name: 'Volvo' },
+      { id: 2, name: 'Saab' },
+      { id: 3, name: 'Opel' },
+      { id: 4, name: 'Audi' },
+  ];
+  onSelectChange(e:any){
+    console.log(e);
+  }
+  onDatePickerChange(e:any){
+    console.log(e);
+    console.log(this.selected.startDate.toDate());
+    console.log(this.selected.startDate.toDate().getTime());
+  }
+  onChangeIp(){
+    console.log(matches(this.ip, "192.168.0.1/32"));
+  }
+  validateIp(){
+    try{
+      console.log(matches(this.ip, this.ipmask));
+    }catch(e){
+      console.log('incorrect IP');
+    }
+  }  
+  validateIpMask(){
+    try{
+      let block = new Netmask(this.ipmask);
+      console.log(block);
+    }catch(e){
+      console.log('incorrect IP mask');
+    }
+    try{
+      let block = getMatch(this.ipmask)      
+      console.log(block);
+    }catch(e){
+      console.log('incorrect IP mask');
+    }
+  }  
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { NgxDaterangepickerMd } from 'ngx-daterangepicker-material';
 import { AppComponent } from './app.component';
+import { NgSelectModule } from '@ng-select/ng-select';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgxDaterangepickerMd.forRoot(),
+    NgSelectModule, 
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
